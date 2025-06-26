0adeb157a4a17f23aba254249871a342c95e8af8
Manish Kumar <46148867+makmaddy@users.noreply.github.com>
2025-06-26 21:49:20 +0530

    E1P4

--- E1/E1P4/App.js
+++ E1/E1P4/App.js
@@ -1,3 +1,25 @@
-const heading = React.createElement("h1",{id:"headingid"},"Using React Heading")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+// const heading = React.createElement("h1",{id:"headingid"},"Using React Heading")
+
+//nesting
+
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"Nested H1 tag")))
+
+//siblings
+
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Nested H1 tag"),React.createElement("h2",{},"Sibling H2 tag")]))
+
+//what happens if we have multiple siblings
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Nested H1 tag"),
+    React.createElement("h2", {}, "Sibling H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Nested H1 tag"),
+    React.createElement("h2", {}, "Sibling H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
