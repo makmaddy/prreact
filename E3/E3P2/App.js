d80247250a956568ef212b10acc52b80eb078dcc
Manish Kumar <46148867+makmaddy@users.noreply.github.com>
2025-07-03 16:57:33 +0530

    add jsx

---
+++ E3/E3P2/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+// const heading = React.createElement("h1",{},"React H1")
+
+const JSXHeading = <h1>React JSX</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(JSXHeading)
