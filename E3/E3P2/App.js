4b165a5e0d0578e5e65c8736e29bb49056d700a7
Manish Kumar <46148867+makmaddy@users.noreply.github.com>
2025-07-11 19:51:07 +0530

    test

--- E3/E3P2/App.js
+++ E3/E3P2/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement("h1",{},"React H1")
 
-const JSXHeading = <h1>React JSX</h1>
+// () for writing in multiple lines
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const JSXHeading = <h1>React JSX</h1>;
 
-root.render(JSXHeading)
+//
+const ReactFunctionalComponent = () => {
+  return (
+    <div>
+      <h1>helllooo</h1>
+    </div>
+  );
+};
+
+const ReactFunctionalComponent2 = () => (
+  <div>
+    <h1>helllooo test</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<ReactFunctionalComponent2 />);
+
+
